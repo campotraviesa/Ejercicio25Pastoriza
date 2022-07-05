94c012fe87a87e21d5106cb4a91b713e4cc2d0a9
Violeta <Violeta@NEGRI>
2022-07-05 14:30:33 -0300

    Agregar archivos de proyecto.

---
+++ Ejercicio25Pastoriza/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Ejercicio25Pastoriza
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Analizar letra como vocal o consonantes";
+            Console.WriteLine("Ingrese la letra que desea analizar");
+            char letra = char.Parse(Console.ReadLine());
+
+        if (letra == 'a' || letra == 'e' || letra == 'i' || letra == 'o' || letra == 'u')
+           {
+                Console.WriteLine("La letra ingresada es una vocal");
+            }
+        else 
+        Console.WriteLine ("NO es una vocal");
+    }
+    }
+}
